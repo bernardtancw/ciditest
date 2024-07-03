e950c35eb5b44b1850d3f30ef78a5cb794880b8c
bernardtancw <bernard.tan91@gmail.com>
2024-07-03 15:28:38 +0800

    Update question.js

--- question.js
+++ question.js
@@ -1708,7 +1708,7 @@ questions = [
   {
     number: "D62_3",
     question: `(RB, PG 6-8) For the next questions I need you to think about the period of {D22dtime} or more
-    during the past 12 months when your {D12feeling} was most severe and
+    during the past 12 months when your {D12feelingv2} was most severe and
     frequent. Please read each of the fourteen sets of statements on page 6-8 in your booklet and circle the one
     response for each of the fourteen that best describes how you were during those {D22dtime}. As
     you finish each set, please tell me the number of the statement you have circled.`,
@@ -1718,7 +1718,7 @@ questions = [
   {
     number: "D64",
     question: `(RB, PG 6_8) For the next questions I need you to think about the period of (several days/two weeks) or more
-    during the past 12 months when your {D12feeling} was most severe and
+    during the past 12 months when your {D12feelingv2} was most severe and
     frequent. I'm going to read fourteen series of statements. Please pick the one statement in each series that
     comes closest to your experience during that worst (several days/two weeks). `,
     inputType: "display",
