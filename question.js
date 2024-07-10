4fbf4db1efbd01e739f7815a521fa448683f7b33
bernardtancw <bernard.tan91@gmail.com>
2024-07-10 09:26:42 +0800

    Update question.js

--- question.js
+++ question.js
@@ -4,10 +4,9 @@ questions = [
     inputType: "response",
     question:
       "The first few questions are for background purposes. How old are you?",
-    requireRange = true,
     min: 18,
     max: 85,
-    trigger: D29, 
+    requireRange = true, 
   },
   {
     number: "SC21",
